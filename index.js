ccbc55294642c2e846a8e45139ab3f7e3c401739
mariyamnavila <bibimariyamnavila@gmail.com>
2025-11-29 22:25:14 +0600

    added patch(updateOne) for bookings

--- index.js
+++ index.js
@@ -172,6 +172,22 @@ async function run() {
             res.send(result);
         });
 
+        app.patch('/bookings/:id',async (req,res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedBooking = req.body;
+            console.log(updatedBooking);
+            const updateDoc = {
+                $set: {
+                    checkInDate: updatedBooking.checkInDate,
+                    checkOutDate: updatedBooking.checkOutDate,
+                    specificBookedDates: updatedBooking.specificBookedDates
+                },
+            };
+            const result = await bookingCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        })
+
         app.delete('/bookings/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
